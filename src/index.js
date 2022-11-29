630e6e16c1832faff15fb6748df88346e9e8ed9c
Matthew Phillips <phillips.matthew829@gmail.com>
2022-11-29 15:37:25 -0600

    Import packages and edit Redux store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {Provider} from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import ThunkMiddleware from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import catsReducer from "./features/cats/catsSlice.js";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const composedEnhancer = composeWithDevTools(applyMiddleware(ThunkMiddleware));
+const store = createStore(catsReducer, composedEnhancer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
