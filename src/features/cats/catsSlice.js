dac60bd6bdce2e784f758688217c8c38bae97580
Matthew Phillips <phillips.matthew829@gmail.com>
2022-11-29 15:38:41 -0600

    Set up reducer and action creator

--- src/features/cats/catsSlice.js
+++ src/features/cats/catsSlice.js
@@ -1,3 +1,36 @@
-export function fetchCats() {}
+// Action Creators
+export function fetchCats(){
+    return function (dispatch){
+        dispatch({type: "cats/catsLoading"});
+        fetch("https://learn-co-curriculum.github.io/cat-api/cats.json")
+        .then((response) => response.json())
+        .then((data) => {
+            // Call dispatch and send the cat JSON data to your store
+            dispatch({type: "cats/catsLoaded", payload: data.images});
+        });
+    }
+}
 
-export default function catsReducer() {}
+// Reducers
+const initialState = {
+    entities: [],
+    status: "idle", // idle state
+};
+
+export default function catsReducer(state = initialState, action){
+    switch(action.type){
+        case "cats/catsLoading":
+            return {
+                ...state,
+                status: "loading"
+            };
+        case "cats/catsLoaded":
+            return {
+                ...state,
+                entities: action.payload,
+                status: "idle",
+            };
+        default:
+            return state;
+    }
+}
